a6d5939722f0a5298961334fbbe031d12792ee5c
Philipp von Weitershausen <philipp@weitershausen.de>
2010-01-29 18:03:44 +0000

    Implement preferences.  Just need to hook them up to the actual functionality :)

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,5 @@
-pref("extensions.bartap.boolpref", false);
-pref("extensions.bartap.intpref", 0);
-pref("extensions.bartap.stringpref", "A string");
+pref("extensions.bartap.tapRestoredTabs", true);
+pref("extensions.bartap.tapBackgroundTabs", false);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.bartap@philikon.de.description", "chrome://bartap/locale/overlay.properties");
