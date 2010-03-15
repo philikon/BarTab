951a57c0343b54e93ebf66e9fc14f7b28ab49ad5
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-15 23:53:32 +0000

    Ooops, if that's not there the menu item won't appear...

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,6 +3,7 @@ pref("extensions.bartap.tapBackgroundTabs", false);
 pref("extensions.bartap.tapAfterTimeout", false);
 pref("extensions.bartap.timeoutValue", 2);
 pref("extensions.bartap.timeoutUnit", 3600);
+pref("extensions.bartap.hostWhitelist", "");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.bartap@philikon.de.description", "chrome://bartap/locale/overlay.properties");
