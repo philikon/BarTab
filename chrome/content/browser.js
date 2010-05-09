philikon/BarTab

commit 5564ed9ce0d7354583e6906a26b36d5662e099ba

Thanks to tab.linkedBrowser.currentURI now pointing to the right thing, we can allow this menu item even on unloaded tabs.

chrome/content/browser.js
@@ -197,11 +197,6 @@ var BarTap = {
 
     if (tab.getAttribute("ontap") == "true") {
       putontap.setAttribute("disabled", "true");
-
-      // TODO even though the tab is unloaded one still might want to
-      // put the host on the whitelist.
-      neverputontap.setAttribute("hidden", "true");
-      return;
     }
 
     let host;

commit 949223d7fa99ff384dc1b8e53496ccefb2e8b4a7

Fix regression introduced in 5564ed9ce0d7354583e6906a26b36d5662e099ba: If the tab is unloaded, disable the Unload menu item.

chrome/content/browser.js
@@ -175,10 +175,6 @@ var BarTap = {
     var neverunload = document.getElementById("context_BarTabNeverUnload");
     var unloadtab = document.getElementById("context_BarTabUnloadTab");
 
-    if (tab.getAttribute("ontap") == "true") {
-      unloadtab.setAttribute("disabled", "true");
-    }
-
     let host;
     try {
       host = tab.linkedBrowser.currentURI.host;
@@ -193,12 +189,17 @@ var BarTap = {
     let label = this.l10n.getFormattedString('neverPutOnTap', [host]);
     neverunload.setAttribute("label", label);
     neverunload.removeAttribute("hidden");
-    if (BarTabUtils.getHostWhitelist().indexOf(host) == -1) {
-      neverunload.removeAttribute("checked");
-      unloadtab.removeAttribute("disabled");
-    } else {
+    if (BarTabUtils.whiteListed(tab.linkedBrowser.currentURI)) {
       neverunload.setAttribute("checked", "true");
       unloadtab.setAttribute("disabled", "true");
+      return;
+    }
+
+    neverunload.removeAttribute("checked");
+    if (tab.getAttribute("ontap") == "true") {
+      unloadtab.setAttribute("disabled", "true");
+    } else {
+      unloadtab.removeAttribute("disabled");
     }
   },
 