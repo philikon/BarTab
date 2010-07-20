f94c98efa44b6903ac9cc6eecae6692121d8ab34
Philipp von Weitershausen <philipp@weitershausen.de>
2010-07-20 14:52:09 +0200

    Avoid accessing gBrowser too early, thereby triggering XBL constructors too early.

--- content/browser.js
+++ content/browser.js
@@ -1,8 +1,10 @@
 Components.utils.import("resource://bartab/prototypes.js");
 
-// Initialize BarTab as soon as possible.
-window.addEventListener("DOMContentLoaded", function() {
-    window.removeEventListener("DOMContentLoaded", arguments.callee, false);
+// Initialize BarTab as soon as possible while ensuring that XBL
+// constructors have already been called.  That means we can't listen
+// to the DOMContentLoaded event.
+window.addEventListener("load", function() {
+    window.removeEventListener("load", arguments.callee, false);
     BarTabUtils.migratePrefs();
     var tabbrowser = document.getElementById("content");
     (new BarTabHandler).init(tabbrowser);
